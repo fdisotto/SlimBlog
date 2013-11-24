db0b7c0a7672e5bfc80aecc91febc8d262d4747a
silentworks <a.smith@silentworks.co.uk>
2013-11-24 22:23:40 +0000

    Removed capsule from project as its no longer supported Added in illuminate Database Changed methods in Users model to be static

--- models/Users.models.php
+++ models/Users.models.php
@@ -3,12 +3,12 @@
 class Users extends Illuminate\Database\Eloquent\Model {
     protected $table = "users";
 
-    public function get_author($id) {
+    public static function get_author($id) {
         $user = Users::find($id);
         return $user->username;
     }
 
-    public function get_id($user) {
+    public static function get_id($user) {
         $user = Users::where('username', '=', $user)->first();
         return $user->id;
     }

8e7ba2450677b4f22b355e4c2adc16061d21ded0
mnlg_ <ivanrise@gmail.com>
2014-02-13 12:41:58 +0100

    fix pagination links

--- routes/base.route.php
+++ routes/base.route.php
@@ -1,5 +1,9 @@
 <?php
 $app->get('/(:page)', function($page = 1) use ($app, $settings) {
+    $p = Posts::count();
+    $pages = ceil($p / $settings->post_per_page);
+    if ($page > $pages) $app->pass();
+
     $posts = Posts::orderBy('creation', 'desc')->skip($settings->post_per_page * ($page - 1))->take($settings->post_per_page)->get();
     $arr = array(); //Posts
     foreach ($posts as $post) {
@@ -19,9 +23,7 @@ $app->get('/(:page)', function($page = 1) use ($app, $settings) {
             $arr[] = $post;
         }
     }
-    $p = Posts::count();
 
-    $pages = ceil($p / $settings->post_per_page);
 
     $app->render('posts.html', array('posts' => $arr, 'pages' => $pages, 'page' => $page));
 })->conditions(array('page' => '\d+'));

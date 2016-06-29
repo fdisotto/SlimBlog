a556c794e1e3a5bba4d58f844544f899547e340f
Alex SZILAGYI <alex.szilagyi@gmail.com>
2016-06-29 21:08:39 +0300

    Fixed updating an article issue

--- src/models/Posts.models.php
+++ src/models/Posts.models.php
@@ -4,6 +4,8 @@ class Posts extends Illuminate\Database\Eloquent\Model {
     protected $table = "posts";
     public $timestamp = false;
 
+    protected $fillable = ['title','text'];
+
     public function comments() {
         return $this->hasMany('Comments');
     }
